0e9a2aaa5c2ecbb52af196f0b8989e8d97f5cec6
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-15 19:56:34 -0500

    fixed css hover

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,12 +5,12 @@ const Projects = ({ profile }) => {
     <div>
       <section id="portfolio">
         <div>
-            <div className="page-section bg-primary">
+            <div className="page-section bg-primary container-fluid">
               <div className="row no-gutters">
                 {profile.projects.map(p => (
-                <div key={p.name} className="col-lg-4 col-sm-6 container">
+                <div key={p.name} className="col-lg-4 col-sm-6">
                   <a className="portfolio-box" href={p.githubUrl}>
-                    <img className="img-fluid" src={p.images.map(i => i.resolutions.desktop.url)} alt="" />
+                    <img className="img-fluid" src={p.images.map(i => i.resolutions.desktop.url)} alt={p.name} />
                     <div className="portfolio-box-caption overlay">
                       <div className="project-name overlay-text">{p.name}</div>
                       <div className="project-category text-white-50 overlay-text">{p.primaryLanguage}</div>
