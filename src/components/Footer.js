6a4c4bff0e731c1f49c3a31cb6fe0ff589a749db
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-15 20:06:07 -0500

    added a couple of icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div>
       <footer className="bg-light py-5">
         <div className="container">
-          <div className="small text-center text-muted">Copyright © 2020 - Code Ninja/Bryant Richards</div></div>
+          <div className="small text-center text-muted">Copyright © 2020 - Code Ninja/Bryant Richards <i class="far fa-hand-spock"></i></div></div>
       </footer>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div className="container">
-          <NavLink className="navbar-brand" to="/">Code Ninja</NavLink><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+          <span class="fas fa-user-ninja"></span><NavLink className="navbar-brand" to="/">Code Ninja</NavLink><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto my-2 my-lg-0">
               <li className="nav-item"><NavLink className="nav-link" to="/about">About</NavLink></li>
