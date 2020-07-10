424dd655756163f005963b9ec0b8435083db792d
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-10 09:53:26 -0500

    fixed issue with resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,8 +10,8 @@ function Resume() {
             <div className="col-lg-8 text-center">
               <h2 className="text-white mt-0">My Resume</h2>
               <hr className="divider light my-4" />
-              <NavLink className="text-white-50 mb-4" to="https://gitconnected.com/bryantit/resume"
-                target="_blank" download>Download Resume</NavLink>
+              <a className="text-white-50 mb-4" href="https://gitconnected.com/bryantit/resume"
+                target="_blank" rel="noopener noreferrer" download>Click here to view resume</a>
             </div>
           </div>
         </div>
